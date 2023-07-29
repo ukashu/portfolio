da3e2881db12ab27121bf4c35f19bb96e18ad343
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-29 11:06:48 +0200

    centered tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,8 +1,8 @@
 export default function Tooltip({ message, children }: { message: string; children: React.ReactNode }) {
   return (
-    <div className="text group relative flex">
+    <div className="text group relative flex items-center justify-center">
       {children}
-      <span className="text-white absolute left-0 top-10 z-[1000] scale-0 rounded p-2 text-center text-xs backdrop-blur transition-all group-hover:scale-100">
+      <span className="text-white absolute top-10 z-[1000] scale-0 rounded p-2 text-center text-xs backdrop-blur transition-all group-hover:scale-100">
         {message}
       </span>
     </div>
