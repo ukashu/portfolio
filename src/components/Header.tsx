03a4e1226e3ef103fcc8d9002244f7cc13502c7f
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-08-03 16:18:38 +0200

    minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
-import { SiGithub } from "react-icons/si/index.js"
+import { SiGithub, SiLinkedin } from "react-icons/si/index.js"
 
 export default function Header() {
   return (
     <header className="min-h-1/5 z-10 flex flex-row items-center justify-evenly gap-1 pb-20 sm:px-32">
-      <a href="https://github.com/">
+      <a href="https://github.com/" className=" flex flex-row items-center justify-around gap-2">
         <SiGithub size="1.5em" onClick={() => console.log("clicked")} />
+        <strong className=" text-base font-semibold">GitHub</strong>
       </a>
-      <p className=" text-base">johndoe@gmail.com</p>
-      <strong className=" text-lg font-semibold">
-        <p>+76 123456789</p>
-      </strong>
+      <a href="https://github.com/" className=" flex flex-row items-center justify-around gap-2">
+        <SiLinkedin size="1.5em" onClick={() => console.log("clicked")} />
+        <strong className=" text-base font-semibold">LinkedIn</strong>
+      </a>
       <p className=" text-lg">
         <button onClick={() => console.log("hellolololo")}>ENG</button> / <button>PL</button>
       </p>
--- src/sections/GoodTeacherShowcase.tsx
+++ src/sections/GoodTeacherShowcase.tsx
@@ -21,7 +21,7 @@ import Tooltip from "../components/Tooltip"
 export default function GoodTeacherShowcase() {
   return (
     <div className="left-0 top-0 flex min-h-[95vh] flex-1 flex-col justify-between gap-1 p-1 text-gt-blue-dark max-lg:pb-10 md:flex-row lg:gap-10 lg:p-10">
-      <div className="w-100% flex max-h-[90vh] flex-row flex-wrap items-center justify-around rounded-3xl bg-opacity-40 md:w-8/12 lg:gap-x-20">
+      <div className="w-100% flex max-h-[90vh] flex-row flex-wrap items-center justify-around rounded-xl bg-opacity-40 md:w-8/12 lg:gap-x-20 lg:rounded-3xl">
         <img
           src={register}
           className="w-auto object-contain drop-shadow-lg duration-200 ease-in hover:z-30 hover:scale-150 max-md:max-w-[10%] md:max-h-[40%] md:rounded-xl"></img>
@@ -38,9 +38,9 @@ export default function GoodTeacherShowcase() {
           src={tasks}
           className="w-auto object-contain drop-shadow-lg duration-200 ease-in hover:z-30 hover:scale-150 max-md:max-w-[10%] md:max-h-[40%] md:rounded-xl"></img>
       </div>
-      <div className="w-100% flex flex-col justify-between gap-12 rounded-3xl md:w-4/12">
+      <div className="w-100% flex flex-col justify-between gap-12 rounded-xl md:w-4/12 lg:rounded-3xl">
         <img src={gt_logo} className=" w-full rounded-3xl"></img>
-        <div className=" w-100% flex flex-grow flex-col justify-around rounded-3xl bg-gt-blue-light bg-opacity-80 p-2 lg:p-6">
+        <div className=" w-100% flex flex-grow flex-col justify-around rounded-xl bg-gt-blue-light bg-opacity-80 p-2 lg:rounded-3xl lg:p-6">
           <div>
             <h1 className=" pb-6 text-center text-xl font-semibold">Online student notebook replacement.</h1>
             <p className=" text-base">
@@ -70,7 +70,7 @@ export default function GoodTeacherShowcase() {
             <SiGithub size="2em" />
           </Tooltip>
         </div>
-        <div className=" w-100% flex flex-col gap-3 rounded-3xl bg-gt-blue-light bg-opacity-80 p-2 lg:p-6">
+        <div className=" w-100% flex flex-col gap-3 rounded-xl bg-gt-blue-light bg-opacity-80 p-2 lg:rounded-3xl lg:p-6">
           <h1 className=" font-semibold">Tech used:</h1>
           <div className=" flex flex-grow flex-row flex-wrap items-center justify-center gap-3">
             <Tooltip message="TypeScript">
